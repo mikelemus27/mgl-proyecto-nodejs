743b62eea4165acfe9ee5268b2e9c8c15742036f
diegoc327 <diegoc327@hotmail.com>
2014-02-27 20:08:35 -0600

    Delete main2.js

--- diego/main2.js
+++
@@ -1,7 +0,0 @@
-process.stdin.setEncoding('utf8');
-
-process.stdin.on('readable', function(chunk){
-  var nombre = process.stdin.read("nombre");
-  var edad = process.stdin.read("edad: ");
-  console.log("nombre",nombre);
-  });
