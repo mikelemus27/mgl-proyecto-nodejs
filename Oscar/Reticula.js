c8d42a078f08c2c2c7275ac0b4df55ced5cca86d
Oscar Alejandro Borden Angeles <ozcar.89@hotmail.com>
2014-03-10 19:22:22 -0600

    10-marzo-2014-OABA

--- Oscar/Reticula.js
+++ Oscar/Reticula.js
@@ -119,10 +119,34 @@ var Entidad = Entidad || {};
   datos.setCreditos(valCreditos);
   datos.setPeriodo(valPeriodo);
   
+  
+     dato("",/.+/,function(valA) {
+	
+           
+	  datos.setClave(valA);
+    process.exit();
+    });
+  
   console.log("Se guardo exitosamente..");
 }
 
-
+ function dato(question, format, callback) {
+			var stdin = process.stdin, stdout = process.stdout;
+ 
+				stdin.resume();
+				stdout.write(question+":");
+ 
+				stdin.once('data', function(data) {
+				data = data.toString().trim();
+ 
+				if (format.test(data)) {
+					callback(data);
+					} else {
+					stdout.write("It should match: "+ format +"\n");
+						dato(question, format, callback);
+							}
+													});
+														}
 
  
   Entidad.Vista = Vista;
