f30766d17fd3877953f276565e9cdf3ad84c0332
Miguel Angel Gallardo Lemus <miguel.gallardo.lemus@gmail.com>
2014-03-21 15:22:32 -0600

    mgl;pchome;21-03-2014-1520;agreagando _01_lemus_html5 examples  a develop branch

---
+++ lemus/_01_lemus_html5/_00_knockoutjs_examples/_01_knockoutjs_example.1/viewmodel.js
@@ -0,0 +1,15 @@
+var AppSpace = AppSpace || {};
+ // This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
+        AppSpace.AppViewModel=function () {
+            this.firstName = ko.observable("Bert");
+            this.lastName = ko.observable("Bertington");
+        
+            this.fullName = ko.computed(function() {
+                return this.firstName() + " " + this.lastName();    
+            }, this);
+        
+            this.capitalizeLastName = function() {
+                var currentVal = this.lastName();        // Read the current value
+                this.lastName(currentVal.toUpperCase()); // Write back a modified value
+            };    
+        };
