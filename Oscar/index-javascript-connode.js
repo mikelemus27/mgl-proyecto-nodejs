7d7c1377f5ea51599566a6de3909cc3645d6dc08
Oscar Alejandro Borden Angeles <ozcar.89@hotmail.com>
2014-03-24 20:31:53 -0600

    sss

--- Oscar/index-javascript-connode.js
+++ Oscar/index-javascript-connode.js
@@ -1,5 +1,14 @@
 var http = require('http');
+var url = require ("url");
+
 http.createServer(function (req, res) {
+
+var pathname = url.parse(req.url).pathname;
+console.log(pathname+ " Esto se ve en node");
+
+if(pathname == "/"){
+
+
  res.writeHead(200, {'Content-Type': 'text/html'});
  res.write(
  '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">'+
@@ -50,6 +59,14 @@ http.createServer(function (req, res) {
   '</script>'+
     '</body>'+
 '</html>');
+}
+ else if(pathname == "/libreria.js"){
+ 	
+ 	res.writeHead(200, ('Content Type'; 'text/javascript'));
+ 	res.write("mensaje = function() {alert ('hola');}");
+ }
+ 
+ 
  res.end();
 }).listen(8888,'127.0.0.1');//.listen(8888, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:8888');
