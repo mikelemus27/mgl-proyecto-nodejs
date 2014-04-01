b080fbd33b4d8ce3bdb266c9101ab599a461a1e0
Miguel Angel Gallardo Lemus <miguel.gallardo.lemus@gmail.com>
2014-03-31 22:25:43 -0600

    mgl;lab06;31-03-2014; terminado el formulario con post

--- lemus/_01_lemus_html5/form_html_stati_server/index.js
+++ lemus/_01_lemus_html5/form_html_stati_server/index.js
@@ -1,9 +1,11 @@
 var http = require("http");
 var querystring= require("querystring");
+var utils= require("util");
 var port = 3000;
 var serverUrl = "localhost";
 
-var server = http.createServer(function(req, res) {
+var server = http.createServer(function(req, res) 
+{
 
   console.log("Request: " + req.url);
   
@@ -25,11 +27,33 @@ var server = http.createServer(function(req, res) {
       
       // parse the received body data
       var decodedBody = querystring.parse(fullBody);
-      //var post=JSON.parse(decodedBody)
- 
+
+      //var post=JSON.eval(utils.inspect(decodedBody));
+      var post= JSON.stringify(decodedBody);
+          post=JSON.parse(post) ;
       // output the decoded data to the HTTP response          
       res.write('<html><head><title>Post data</title></head><body><pre>');
       //res.write(utils.inspect(decodedBody));
+      res.write("proibando el servidor");
+      res.write("</br>");
+      res.write(utils.inspect(decodedBody));
+      res.write("</br>");
+      //console.log(post.data);
+      //res.write(post.data.toString());
+         res.write("</br>");
+         txt1= post.num1.toString();
+          txt2= post.num2.toString();
+          result=parseInt(txt1)+parseInt(txt2);
+      res.write(txt1);
+      res.write("</br>");
+      res.write("+");
+       res.write("</br>");
+       res.write(txt2);
+           res.write("</br>");
+      res.write("=");
+       res.write("</br>");
+       res.write("</br>");
+        res.write(result.toString());
       res.write('</pre></body></html>');
       
       res.end();
@@ -40,9 +64,10 @@ var server = http.createServer(function(req, res) {
     res.writeHead(405, "Method not supported", {'Content-Type': 'text/html'});
     res.end('<html><head><title>405 - Method not supported</title></head><body><h1>Method not supported.</h1></body></html>');
   }
-  res.end(html);
+ // res.end(html);
 
 });
 
 console.log("Listening at " + serverUrl + ":" + port);
-server.listen(process.env.PORT, process.env.IP);//.listen(8888, '127.0.0.1');
+//server.listen(process.env.PORT, process.env.IP);//.listen(8888, '127.0.0.1');
+server.listen(8888,'127.0.0.1');
