456e268a1250b7e12e08745c3b415c6e02f73f38
Miguel Angel Gallardo Lemus <miguel.gallardo.lemus@gmail.com>
2014-05-07 12:53:35 -0500

    mgl;JCSlab;07-05-2014;_08_knockoutexample.1;exito;que funcione en localhojs  $.ajax  y json  con express

--- lemus/_01_lemus_html5/_01_knockoutjs_examples/_08_knockoutjs_example.1/app.js
+++ lemus/_01_lemus_html5/_01_knockoutjs_examples/_08_knockoutjs_example.1/app.js
@@ -4,14 +4,15 @@
  * *************************************************************************************
  * http://blog.greatrexpectations.com/2013/01/23/single-page-applications-using-node-knockout/
  * http://stackoverflow.com/questions/18310394/no-access-control-allow-origin-node-apache-port-issue
- *http://stackoverflow.com/questions/19740613/origin-http-localhost-is-not-allowed-by-access-control-allow-origin
- *http://stackoverflow.com/questions/1969476/ajax-success-event-not-working
+ * http://stackoverflow.com/questions/19740613/origin-http-localhost-is-not-allowed-by-access-control-allow-origin
+ * http://stackoverflow.com/questions/1969476/ajax-success-event-not-working
+ * http://stackoverflow.com/questions/5892569/responding-with-a-json-object-in-nodejs-converting-object-array-to-json-string
  ***************************************/
 var express = require('express');
-cors = require('cors')
-
-/**************************************************/
+var cors = require('cors')
 var _ = require('underscore');
+/**************************************************/
+
 var app = express();
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
@@ -64,22 +65,27 @@ app.get('/hello.txt', function(req, res){
 });
 
 app.post('/form-test.html', function(req, res) {
-
-	console.log("in form-test");
+  var num1="";
+  var num2="";
+  var result="";
+  var objToJson=0;
+  var json="";
+  //console.log("in form-test");
 	res.writeHead(200, {'Content-Type': 'application/json'});
 
     //res.write("entrando en el post"+req.body.num1);
-  //  res.write();
 
-    var num1 = req.body.num1;
-    var    num2 = req.body.num2;
+  num1 = req.body.num1;
+  num2 = req.body.num2;
   num1=  num1.toString();
   num2=  num2.toString();
-  var result=parseInt(num1)+parseInt(num2);
-
-// res.write ("el num1 es : "+num1+" </br> el num2 es : "+num2+"</br>"+"lasuma es : "+result);
- res.write("{ num1:"+ num1+", num2:"+num2 +", result:"+result+" }");
- console.log ("el num1 es : "+num1+" </br> el num2 es : "+num2+"</br>"+"lasuma es : "+result);
+  result=parseInt(num1)+parseInt(num2);
+  console.log ("el num1 es : "+num1+" </br> el num2 es : "+num2+"</br>"+"lasuma es : "+result);
+  // res.write ("el num1 es : "+num1+" </br> el num2 es : "+num2+"</br>"+"lasuma es : "+result);// cuando el resultado no es  json
+  objToJson = { num11: num1, num22: num2,res:result };
+  json = JSON.stringify(objToJson); 
+  res.write(json);
+  //res.end (json);//se puede  utilizar en lugar de  res.write
   res.end ();
 });
 /*var server = app.listen(3000, function() {
@@ -87,9 +93,9 @@ app.post('/form-test.html', function(req, res) {
 });*/
 
 /*app.listen(process.env.PORT,process.env.IP); */
-app.listen(8888,"localhost"); 
-
-console.log("port"+process.env.PORT);
-console.log("ip"+process.env.IP);
-//app.listen(process.env.PORT,process.env.IP); 
+app.listen(8888,"localhost"); //solo para localhost
+console.log('Listening on  http://localhost:8888/');//solo para localhost
+//console.log("port"+process.env.PORT);//solo para  c9.io
+//console.log("ip"+process.env.IP);//solo para c9.io
+//app.listen(process.env.PORT,process.env.IP); //solo para c9.io
 
