ea2e86d70cf20a7b68c9ceafc6ecdf0a3b97b4e1
Ulises Cuauhtemoc Rodriguez Sahagun <ulisestheavenger@gmail.com>
2014-05-20 13:49:09 -0500

    nueva carpeta

--- lemus/_00_nodejs/_00_nodejs_web/_03_hellohttp3/index.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-    
-http.createServer(function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end("Switch On The Code Rocks My Socks Off!"); 
-}).listen(process.env.PORT, process.env.IP);//.listen(8888, '127.0.0.1');
