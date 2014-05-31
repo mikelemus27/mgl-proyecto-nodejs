9cc7d1eb5733486a0d6fc9de9c83aaefb372da77
luyz2000 <luis2000_k999@hotmail.com>
2014-05-30 19:11:54 -0600

    as
    assdasd

--- my_common_library.js
+++ my_common_library.js
@@ -7,3 +7,26 @@ this.fld_txt_inp.className= //define  class nane through attributes
 
 this.fld_txt_inp.style.width= //compute length
 this.fld_txt_inp.naxLength = t_mx_ln; //assign max len
+
+
+
+
+
+
+//3 parte by Luis
+this.fid_txt_inp.onblur=function(){
+var _rg_zr=/^0+/;
+var tmpval=(removeNumberFormat(self.fld_txt_inp.value)).replace(_rg_zr,'');
+tmpval=(tmpval=='')?'0':tmpval;
+self.fld_txt_inp.value=addNumberFormat(tmpval);
+
+};
+
+this.fld_txt_inp.onkeypress=funtion(eve){
+var rg=/^([0-9,\b])/;
+var kn=window.event?event.keyCode : eve.which;
+kc=String.fromharCode(kn);
+return rg.test(kc);
+};
+var self=this;
+args_cus_obj.appendChild(this.fld_txt_inp);
