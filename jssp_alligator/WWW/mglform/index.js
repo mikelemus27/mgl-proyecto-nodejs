46548816304aad0f7550b5787382cfc4c0763953
miguel gallardo lemus <mike@US1210Temp.tecvallarta>
2014-06-03 11:57:06 -0500

    mgl;03-06-2014;linuxSind;exito; recurperado ejemplo mglform  alligator

---
+++ jssp_alligator/WWW/mglform/index.js
@@ -0,0 +1,4 @@
+(function(log,lib,application,request,responseHead,commands,session) {commands.writeEscapedText("");
+	commands.forward("mglform/form.jssp");
+commands.writeEscapedText("%0D%0A");
+})
