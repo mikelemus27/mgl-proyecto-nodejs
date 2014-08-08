e588bb233ffe0e5ee68fcf039b9caec9e6d8cd3e
miguel gallardo <miguel.gallardo.lemus@gmail.com>
2014-08-08 01:51:09 +0000

    pribando

---
+++ Custom_tags_html/custom_imput/my_common_library.js
@@ -0,0 +1,57 @@
+/*pegar  aqui el codigo*/
+
+
+
+/*primera parte*/
+
+function myfield(args_cus_Obj)
+{
+if((typeof args_cus_Obj.attributes.typ !='undefined')  && args_cus_Obj.attributes.typ!==null)
+    {
+          this._fieldtype=args_cus_Obj.attributes.typ.value;
+	 }
+        var ev_att=args_cus_Obj.attributes.callback;
+        this._callbk_click=(typeof ev_att !='undefined' && ev_att!==null)?ev_att.value:null;
+	 
+        var t_mx_ln= parseInt(args_cus_Obj.attributes.len.value,10);
+        /*Segunda parte */
+        this.fld_txt_inp=document.createElement("input");//create an input field
+        this.fld_txt_inp.type= "text";
+        this.fld_txt_inp.className= "prueba";//define  class nane through attributes
+
+        this.fld_txt_inp.style.width= "20px";//compute length
+        this.fld_txt_inp.naxLength = t_mx_ln; //assign max len
+        //3 parte by Luis
+        this.fid_txt_inp.onblur=function()
+        {
+        var _rg_zr=/^0+/;
+        var tmpval=(removeNumberFormat(self.fld_txt_inp.value)).replace(_rg_zr,'');
+        tmpval=(tmpval=='')?'0':tmpval;
+        self.fld_txt_inp.value=addNumberFormat(tmpval);
+        };
+
+        this.fld_txt_inp.onkeypress=funtion(eve){
+        var rg=/^([0-9,\b])/;
+        var kn=window.event?event.keyCode : eve.which;
+        kc=String.fromharCode(kn);
+        return rg.test(kc);
+        };
+        var self=this;
+        args_cus_obj.appendChild(this.fld_txt_inp);
+}
+//4 parte_Eduardo
+function initializeUI(){
+	 var arr= document.getElementsByTagName("+");
+	 
+	 for(var i=0; i<arr.length;i++){
+		var tgNm= arr.item(i).nodeName;
+		var obj=null;
+		if(tgNm.toLowerCase()=='myfield'){
+			obj=new myfield(arr.item(i));
+		}
+		else if (tgNm.toLowerCase()=='myselect'){
+			obj=new myselect(arr.item(i));
+		}
+		...
+	}//end of for loop
+}
