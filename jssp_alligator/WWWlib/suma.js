4e41d836f8b4a0b05e745080952334b9c58153f3
miguel gallardo <miguel.gallardo.lemus@gmail.com>
2015-06-02 02:02:32 +0000

    Script = process.binding('evals').Script,//esta linea  genra  error  con node v 0.12.2

--- jssp_alligator/WWWlib/suma.js
+++ jssp_alligator/WWWlib/suma.js
@@ -1,6 +1,8 @@
 exports.sumar=function(num1,num2)
 {
 	var result=0;
-	result=num1+num2;
+	console.log("lib.suma; param1="+ num1);
+	console.log("lib.suma; param2="+ num2);
+	result=parseInt(num1)+parseInt(num2);
 	return result;
 }
